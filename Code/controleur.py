1568f6875a7cec40367781b3a935f71bc415a188
hadj ali <hadjalitakwa7@gmail.com>
2025-02-17 20:06:35 +0100

    importation de la bobliotheque logging

--- Code/controleur.py
+++ Code/controleur.py
@@ -1,18 +1,21 @@
+import logging
 from environnement import Environnement
 
 class Controleur:
     def __init__(self, vitesse_gauche, vitesse_droite, mode):
-        """
-        Initialise le contrôleur avec les vitesses des roues et le mode de simulation.
+	"""
+        Initialise le contrôleur avec les paramètres de simulation.
         :param vitesse_gauche: Vitesse de la roue gauche.
         :param vitesse_droite: Vitesse de la roue droite.
-        :param mode: Mode de simulation (automatique, manuel, carré).
+        :param mode: "automatique", "manuel" ou "carré".
+        :param affichage: True pour affichage graphique, False pour console.
+        :param longueur_carre: Longueur du côté du carré (pour le mode carré).
         """
-        self.vitesse_gauche = vitesse_gauche
-        self.vitesse_droite = vitesse_droite
-        self.mode = mode
-        self.env = Environnement(self.vitesse_gauche, self.vitesse_droite, self.mode)
-    
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.logger.setLevel(logging.DEBUG)
+        self.env = Environnement(vitesse_gauche, vitesse_droite, mode, affichage, longueur_carre)
+        self.logger.info("Contrôleur initialisé en mode: %s", mode)
+
     def demarrer_simulation(self):
         """
         Démarre la simulation en fonction du mode choisi.
