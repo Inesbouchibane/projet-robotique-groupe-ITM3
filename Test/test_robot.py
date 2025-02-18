1a75e593ba83ddc973fff33791a4c109047ef5d0
Inesbouchibane <inesbouchibane.de@email.com>
2025-02-18 20:15:31 +0100

    Modification de fichier test_robot.py

--- Test/test_robot.py
+++ Test/test_robot.py
@@ -1,34 +1,31 @@
 import unittest
+import math
 from robot import Robot
 
 class TestRobot(unittest.TestCase):
     def setUp(self):
-        """Initialisation d'un robot avant chaque test."""
-        self.robot = Robot(100, 100, 10, 10)  # Position initiale avec vitesse gauche = droite = 10
+        self.robot = Robot(100, 100, 1, 1)
     
     def test_deplacement(self):
         x_avant, y_avant = self.robot.x, self.robot.y
         self.robot.deplacer()
         self.assertNotEqual((self.robot.x, self.robot.y), (x_avant, y_avant))
-
-
-    def test_limite_fenetre(self):
-        """Test que le robot ne sort pas des limites (0,800)x(0,600)."""
-        self.robot.x = 795
-        self.robot.y = 595
-        self.robot.vitesse_gauche = 10
+        self.assertAlmostEqual(self.robot.x, x_avant + 10, delta=0.1)
+        self.assertAlmostEqual(self.robot.y, y_avant, delta=0.1) 
+        
+    def test_rotation(self):
+        self.robot.vitesse_gauche = 5
         self.robot.vitesse_droite = 10
+        angle_avant = self.robot.angle
         self.robot.deplacer()
-        self.assertLessEqual(self.robot.x, 800)
-        self.assertLessEqual(self.robot.y, 600)
+        self.assertNotEqual(self.robot.angle, angle_avant) 
 
-    
+        
     def test_scan_infrarouge(self):
-        """Test du capteur infrarouge avec un obstacle."""
         obstacles = [(200, 200, 50, 50)]
         point_detecte = self.robot.scan_infrarouge(obstacles, 100)
-        self.assertIsInstance(point_detecte, tuple) # Vérifie que c'est un tuple
-        self.assertEqual(len(point_detecte), 2) # Vérifie qu'il contient bien (x, y)
+        self.assertIsInstance(point_detecte, tuple)
+        self.assertEqual(len(point_detecte), 2)
 
 if __name__ == "__main__":
     unittest.main()
