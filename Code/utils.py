8d61669df19d06f7714ef1e284ea0ad6effd10ec
Inesbouchibane <inesbouchibane.de@email.com>
2025-03-18 21:37:25 +0100

    ajouter une fonction getDistanceFromPts(p1, p2) dans le fichier utils .py

--- Code/utils.py
+++ Code/utils.py
@@ -1,7 +1,11 @@
 import math
 
-def getAngleFromVect(v1, v2):
+   def getAngleFromVect(v1, v2):
     dot = v1[0] * v2[0] + v1[1] * v2[1]
     det = v1[0] * v2[1] - v1[1] * v2[0]
     return math.degrees(math.atan2(det, dot))
+    
+    
+   def getDistanceFromPts(p1, p2):
+    return math.sqrt((p2[0] - p1[0])**2 + (p2[1] - p1[1])**2)
 
