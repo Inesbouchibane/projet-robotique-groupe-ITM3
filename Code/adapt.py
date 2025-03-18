ab42ff333b9638f33c2cf602241759957ba3444d
bouzekrimohamed <lm_bouzekri@esi.dz>
2025-03-18 20:37:32 +0100

    creation de la classe adaptateur

---
+++ Code/adapt.py
@@ -0,0 +1,8 @@
+from utils import getAngleFromVect, getDistanceFromPts
+
+class Adaptateur:
+    def initialise(self): pass
+    def setVitAngA(self, vit): pass
+    def getDistanceA(self): pass
+    def getDistanceParcourue(self): pass
+    def getAngleParcouru(self): pass
