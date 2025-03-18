86c68442d2cb04ee8c056856ebca623692d90cfc
Meriem <meriembrh6@gmail.com>
2025-03-18 09:45:00 +0100

    Ajout des obstacles dans l'environnement et initialisation de l'affichage graphique

--- Code/main.py
+++ Code/main.py
@@ -16,3 +16,8 @@ envi = Environnement(LARGEUR_ENV, LONGUEUR_ENV, SCALE_ENV_1)
 robot1 = Robot("r1", 500, 250, 20, 40, 10, 5, "lightblue")
 adaptateur = Adaptateur_simule(robot1, envi)
 envi.setRobot(adaptateur)
+
+for n, pts in [('R1', LIST_PTS_OBS_RECTANGLE1), ('R2', LIST_PTS_OBS_CARRE), ('R3', LIST_PTS_OBS_RECTANGLE3)]:
+     envi.addObstacle(n, pts)
+
+affichage = Affichage(LARGEUR_ENV, LONGUEUR_ENV, [o.get_bounding_box() for o in envi.listeObs])
