50fb40fd50373752e8f396494b8cf2782174cfdd
bouzekrimohamed <lm_bouzekri@esi.dz>
2025-03-18 20:44:03 +0100

    creation de la Adaptateur_simule

--- Code/adapt.py
+++ Code/adapt.py
@@ -6,3 +6,10 @@ class Adaptateur:
     def getDistanceA(self): pass
     def getDistanceParcourue(self): pass
     def getAngleParcouru(self): pass
+    
+class Adaptateur_simule(Adaptateur):
+    def __init__(self, robot, env):
+        self.robot = robot
+        self.env = env
+        self.last_point = (robot.x, robot.y)
+        self.last_dir = robot.direction
