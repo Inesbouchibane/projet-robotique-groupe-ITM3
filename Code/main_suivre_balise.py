0904b4ee8e98b0b45b9c34b2c01645bc85192921
Inesbouchibane <inesbouchibane.de@email.com>
2025-03-26 00:03:32 +0100

    modification du module main de la balise

--- Code/main_suivre_balise.py
+++ Code/main_suivre_balise.py
@@ -1,25 +1,26 @@
+from robot.robot_mockup import RobotMockup
+from robot.adapt.Adaptateur_simule import Adaptateur_simule
+from environnement.balise import Balise
+from controleur.strategies import StrategieSuivreBalise
+from interface_graphique import Affichage
+from time import sleep
+from utils import TIC_SIMULATION, LARGEUR_ENV, LONGUEUR_ENV
 
+def main():
+    # Initialisation du robot
+    robot = RobotMockup("MockBot", 400, 300, 20, 40, 10, 5, "red")
+    adaptateur = Adaptateur_simule(robot, None)  # Pas d'environnement pour ce test
+    balise = Balise(600, 400)  # Balise à (600, 400)
+    strategie = StrategieSuivreBalise(adaptateur, balise)
+    
+     # Initialisation de l'affichage
+    affichage = Affichage(LARGEUR_ENV, LONGUEUR_ENV, [])
 
+    print("Test de la stratégie Suivre Balise")
+    strategie.start()
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- running = True
+    
+    running = True
     while running:
         strategie.step()
         if strategie.running:  # Ne refresh que si la stratégie est active
@@ -36,4 +37,7 @@
         sleep(TIC_SIMULATION)
 
     affichage.attendre_fermeture()
+
+
+
 
