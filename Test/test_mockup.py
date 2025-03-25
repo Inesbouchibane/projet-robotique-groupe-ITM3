baf446ac1dc715bfc95d5a9574a32ea54bb3e79d
Inesbouchibane <inesbouchibane.de@email.com>
2025-03-25 23:50:20 +0100

    creation du fichier test_mockup pour les test unitaires de notre classe RobotMockup

--- Test/test_mockup.py
+++ Test/test_mockup.py
@@ -8,3 +8,8 @@ class TestRobotMockup(unittest.TestCase):
         self.robot = RobotMockup("TestBot", 0, 0, 10, 20, 15, 5, "red")
     
     @patch("robot_mockup.uniform", return_value=0)
+    
+    def test_refresh_no_noise(self, mock_uniform):
+        self.robot.setVitAng(1.0)
+        self.robot.refresh(1.0)
+        self.assertNotEqual((self.robot.x, self.robot.y), (0, 0))
