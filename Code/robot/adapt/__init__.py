59e5e72237429eba5223ed000fe64d8e0fa4083d
mouna2235677890 <mounabahamid10@gmail.com>
2025-03-25 17:02:34 +0100

    Creation du fichier __init__.py dans notre module adapt

---
+++ Code/robot/adapt/__init__.py
@@ -0,0 +1,6 @@
+from .adapt import Adaptateur
+from .Adaptateur_simule import Adaptateur_simule
+from .Adaptateur_reel import AdaptateurReel
+
+__all__ = ['Adaptateur', 'Adaptateur_simule', 'AdaptateurReel']
+
