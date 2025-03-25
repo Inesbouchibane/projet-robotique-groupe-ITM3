8d904dac3a3233ca37364376ca140eb4a45f3a03
Meriem <meriembrh6@gmail.com>
2025-03-25 22:29:16 +0100

    Implémentation de la fonction getDistance

--- Code/robot/robot_reel.py
+++ Code/robot/robot_reel.py
@@ -9,8 +9,16 @@ class RobotReel(Robot):
         self.capteur_distance = None  # Capteur de distance (à définir)
 
 
-        def setMoteurDPS(self, vitesse_gauche, vitesse_droite):
+    def setMoteurDPS(self, vitesse_gauche, vitesse_droite):
         """Définit la vitesse en degré par seconde des moteurs gauche et droit."""
-            self.vitAngG = vitesse_gauche
-            self.vitAngD = vitesse_droite
+        self.vitAngG = vitesse_gauche
+        self.vitAngD = vitesse_droite
+
+
+    def getDistance(self):
+        """Retourne la distance mesurée par le capteur (simulation du capteur réel)."""
+        if self.capteur_distance:
+            return self.capteur_distance.mesurer_distance()
+        return float('inf')  # Valeur par défaut si aucun capteur n'est présent
+
 
