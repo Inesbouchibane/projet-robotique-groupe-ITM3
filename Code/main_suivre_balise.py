4daeda28d8c4aa4df36137d50fcb70645c51e261
Inesbouchibane <inesbouchibane.de@email.com>
2025-03-25 22:24:12 +0100

    Creation du module simulation et les fichier de simulation 2D et 3D

--- Code/main_suivre_balise.py
+++ Code/main_suivre_balise.py
@@ -25,3 +25,15 @@
         if strategie.running:  # Ne refresh que si la stratégie est active
             robot.refresh(TIC_SIMULATION)
             affichage.mettre_a_jour(robot)
+            
+            
+        # Gestion des événements pour quitter
+        action = affichage.handle_events(adaptateur)
+        if action == "quit":
+            strategie.stop()
+            running = False
+
+        sleep(TIC_SIMULATION)
+
+    affichage.attendre_fermeture()
+
