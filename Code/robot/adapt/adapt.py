8e1e4b7fc1e2725da5113887e7b98f89d9a29d1a
Meriem <meriembrh6@gmail.com>
2025-03-25 17:17:51 +0100

    Implémentation des fonctions du fichier adapt.py

---
+++ Code/robot/adapt/adapt.py
@@ -0,0 +1,21 @@
+from utils import getAngleFromVect, getDistanceFromPts
+
+class Adaptateur:
+    """Classe de base pour les adaptateurs (réel et simulé)."""
+    def initialise(self):
+        pass
+
+    def setVitAngA(self, vit):
+        pass
+
+    def getDistanceA(self):
+        pass
+
+    def getDistanceParcourue(self):
+        pass
+
+    def getAngleParcouru(self):
+        pass
+
+
+
