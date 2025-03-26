c70fde7743429dc0af46a16def0fdbc6b73f3485
mouna2235677890 <mounabahamid10@gmail.com>
2025-03-26 09:47:30 +0100

    modification du main_suivre_balise

--- Code/main_suivre_balise.py
+++ Code/main_suivre_balise.py
@@ -2,7 +2,7 @@ from robot.robot_mockup import RobotMockup
 from robot.adapt.Adaptateur_simule import Adaptateur_simule
 from environnement.balise import Balise
 from controleur.strategies import StrategieSuivreBalise
-from affichage import Affichage
+from interface_graphique import Affichage
 from time import sleep
 from utils import TIC_SIMULATION, LARGEUR_ENV, LONGUEUR_ENV
 
@@ -12,22 +12,21 @@ def main():
     adaptateur = Adaptateur_simule(robot, None)  # Pas d'environnement pour ce test
     balise = Balise(600, 400)  # Balise à (600, 400)
     strategie = StrategieSuivreBalise(adaptateur, balise)
-    
-     # Initialisation de l'affichage
+
+    # Initialisation de l'affichage
     affichage = Affichage(LARGEUR_ENV, LONGUEUR_ENV, [])
 
     print("Test de la stratégie Suivre Balise")
     strategie.start()
 
-    
+    # Boucle principale
     running = True
     while running:
         strategie.step()
         if strategie.running:  # Ne refresh que si la stratégie est active
             robot.refresh(TIC_SIMULATION)
             affichage.mettre_a_jour(robot)
-            
-            
+
         # Gestion des événements pour quitter
         action = affichage.handle_events(adaptateur)
         if action == "quit":
@@ -38,6 +37,5 @@ def main():
 
     affichage.attendre_fermeture()
 
-
-
-
+if __name__ == "__main__":
+    main()
