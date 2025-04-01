054c3ef3c1b938baa3683c1f57de597ca6e766bf
mouna2235677890 <mounabahamid10@gmail.com>
2025-04-01 20:19:09 +0200

    Modification de la classe adaptateur

--- src/robot/adapt/adapt.py
+++ src/robot/adapt/adapt.py
@@ -1,65 +1,58 @@
 from abc import ABC, abstractmethod
+from logging import getLogger
 
 class Adaptateur(ABC):
+    """Interface commune pour tous les adaptateurs robotiques"""
+    
     @abstractmethod
     def initialise(self):
-        pass
-
-    @abstractmethod
-    def setVitAngA(self, vit):
-        pass
-
-    @abstractmethod
-    def setVitG(self, vit):
-        pass
-
-    @abstractmethod
-    def setVitD(self, vit):
-        pass
-
-    @abstractmethod
-    def getDistanceObstacle(self):
-        pass
-
-    @abstractmethod
-    def getVitD(self):
-        pass
-
-    @abstractmethod
-    def getVitG(self):
-        pass
-
-    @abstractmethod
-    def getDistanceParcourue(self):
-        pass
-
-    @abstractmethod
-    def getAngleParcouru(self):
-        pass
-
-    @abstractmethod
-    def getPosition(self):
-        """Retourne la position actuelle (x, y) du robot"""
-        pass
-
-    @abstractmethod
-    def getDirection(self):
-        """Retourne le vecteur direction du robot"""
-        pass
-
-    @abstractmethod
-    def isCrashed(self):
-        """Retourne si le robot est en collision"""
+        """Réinitialise les compteurs et états internes"""
         pass
 
     @abstractmethod
     def setVitAngGA(self, vit):
+        """Définit la vitesse angulaire de la roue gauche (en degrés/s)"""
         pass
 
     @abstractmethod
     def setVitAngDA(self, vit):
+        """Définit la vitesse angulaire de la roue droite (en degrés/s)"""
         pass
 
     @abstractmethod
-    def getDistanceA(self):
+    def getDistanceObstacle(self):
+        """
+        Retourne la distance à l'obstacle le plus proche en mm
+        avec mise en cache pour optimisation
+        """
+        pass
+
+    @abstractmethod
+    def getDistanceParcourue(self):
+        """Retourne la distance totale parcourue depuis l'initialisation (en mm)"""
+        pass
+
+    @abstractmethod
+    def getAngleParcouru(self):
+        """Retourne l'angle total parcouru depuis l'initialisation (en radians)"""
+        pass
+
+    @abstractmethod
+    def getPosition(self):
+        """Retourne la position actuelle sous forme de tuple (x, y) en mm"""
+        pass
+
+    @abstractmethod
+    def getDirection(self):
+        """Retourne le vecteur direction unitaire sous forme [x, y]"""
+        pass
+
+    @abstractmethod
+    def isCrashed(self):
+        """Retourne True si le robot est en état de collision"""
+        pass
+
+    @abstractmethod
+    def arreter(self):
+        """Arrête immédiatement tous les mouvements du robot"""
         pass
