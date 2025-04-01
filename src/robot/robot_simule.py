3010e269ab482892f7c5db42b1f43805f6559187
bouzekrimohamed <lm_bouzekri@esi.dz>
2025-04-01 21:06:01 +0200

    Ajouter getDistanceObstacle() et getDistanceParcouru() dans robot_simule.py

--- src/robot/robot_simule.py
+++ src/robot/robot_simule.py
@@ -2,3 +2,14 @@
         vitesse_angulaire = valeur / (self.taille_roue / 2)  # Correction : rayon, pas diamètre
         self.vitAngG = vitesse_angulaire
         self.vitAngD = vitesse_angulaire
+        
+        
+    
+    
+    
+    
+    def getDistanceParcouru(self):
+        return self.distance_parcourue
+
+    def getDistanceObstacle(self):
+        return float('inf')
