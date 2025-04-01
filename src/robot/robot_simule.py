57af20faf9aa9073cc43a53234e733ae1f2be0a2
bouzekrimohamed <lm_bouzekri@esi.dz>
2025-04-01 21:08:00 +0200

    ajouter refresh dans robot_simule.py

--- src/robot/robot_simule.py
+++ src/robot/robot_simule.py
@@ -19,3 +19,20 @@
 
     def get_VitD(self):
         return self.vitAngD
+        
+  def refresh(self, delta_t=None):
+        if delta_t is None:
+            current_time = time()
+            delta_t = current_time - self.last_update
+            self.last_update = current_time
+        delta_t = max(delta_t, 0.02)  # Garantir un delta_t minimum (TIC_SIMULATION)
+
+        if self.estCrash:
+            self.arreter()
+            return
+
+        rayon_roue = self.taille_roue / 2
+        v_gauche = self.vitAngG * rayon_roue
+        v_droite = self.vitAngD * rayon_roue
+        v = (v_gauche + v_droite) / 2
+        omega = (v_droite - v_gauche) / self.width
