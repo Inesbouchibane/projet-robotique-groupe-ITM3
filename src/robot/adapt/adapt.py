c48c3c244e4a43cb67efdbb8ee436bd7da016ac2
mouna2235677890 <mounabahamid10@gmail.com>
2025-04-01 20:01:32 +0200

    Modification du fichier adapt.py

--- src/robot/adapt/adapt.py
+++ src/robot/adapt/adapt.py
@@ -1,21 +1,65 @@
-from utils import getAngleFromVect, getDistanceFromPts
+from abc import ABC, abstractmethod
 
-class Adaptateur:
-    """Classe de base pour les adaptateurs (réel et simulé)."""
+class Adaptateur(ABC):
+    @abstractmethod
     def initialise(self):
         pass
 
+    @abstractmethod
     def setVitAngA(self, vit):
         pass
 
-    def getDistanceA(self):
+    @abstractmethod
+    def setVitG(self, vit):
         pass
 
+    @abstractmethod
+    def setVitD(self, vit):
+        pass
+
+    @abstractmethod
+    def getDistanceObstacle(self):
+        pass
+
+    @abstractmethod
+    def getVitD(self):
+        pass
+
+    @abstractmethod
+    def getVitG(self):
+        pass
+
+    @abstractmethod
     def getDistanceParcourue(self):
         pass
 
+    @abstractmethod
     def getAngleParcouru(self):
         pass
 
+    @abstractmethod
+    def getPosition(self):
+        """Retourne la position actuelle (x, y) du robot"""
+        pass
 
+    @abstractmethod
+    def getDirection(self):
+        """Retourne le vecteur direction du robot"""
+        pass
 
+    @abstractmethod
+    def isCrashed(self):
+        """Retourne si le robot est en collision"""
+        pass
+
+    @abstractmethod
+    def setVitAngGA(self, vit):
+        pass
+
+    @abstractmethod
+    def setVitAngDA(self, vit):
+        pass
+
+    @abstractmethod
+    def getDistanceA(self):
+        pass
