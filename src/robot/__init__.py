fd9d39640fc9ea227e42d8f7de4dfb68cad1aa48
Meriem <meriembrh6@gmail.com>
2025-04-02 09:15:38 +0200

    Modification de l'init

--- src/robot/__init__.py
+++ src/robot/__init__.py
@@ -1,6 +1,6 @@
 from .adapt import Adaptateur
 from .adapt_simule import AdaptateurSimule
-from .adapt_reel import AdaptateurReel
+from .adapt_reel import Adaptateur_reel
 
-__all__ = ['Adaptateur', 'AdaptateurSimule', 'AdaptateurReel']
+__all__ = ['Adaptateur', 'AdaptateurSimule', 'Adaptateur_reel']
 
