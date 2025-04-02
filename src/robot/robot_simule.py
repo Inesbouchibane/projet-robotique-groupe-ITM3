663a45dd42ce50541bc0e35dc1d288455c6c16f0
mouna2235677890 <mounabahamid10@gmail.com>
2025-04-02 09:25:52 +0200

    correction d une erreur

--- src/robot/robot_simule.py
+++ src/robot/robot_simule.py
@@ -20,29 +20,35 @@ class RobotSimule(Robot):
         self.last_update = time()
         self.estCrash = False
 
-
     def avancer(self, valeur):
         vitesse_angulaire = valeur / (self.taille_roue / 2)  # Correction : rayon, pas diamètre
         self.vitAngG = vitesse_angulaire
         self.vitAngD = vitesse_angulaire
-        
+
     def arreter(self):
         self.vitAngG = 0
-        self.vitAngD = 0 
-    
-    
+        self.vitAngD = 0
+
+    def set_VitG(self, valeur):
+     # Correction : rayon
+        self.vitAngG = valeur
+
+    def set_VitD(self, valeur):
+          # Correction : rayon
+        self.vitAngD = valeur
+
     def getDistanceParcouru(self):
         return self.distance_parcourue
 
     def getDistanceObstacle(self):
         return float('inf')
-        
+
     def get_VitG(self):
         return self.vitAngG
 
     def get_VitD(self):
         return self.vitAngD
-        
+
     def refresh(self, delta_t=None):
         if delta_t is None:
             current_time = time()
@@ -71,8 +77,7 @@ class RobotSimule(Robot):
 
         # Ajout pour débogage
         print(f"vitAngG={self.vitAngG}, vitAngD={self.vitAngD}, v_gauche={v_gauche}, v_droite={v_droite}, omega={omega}, delta_t={delta_t}, theta={theta:.3f}, new_theta={new_theta:.3f}")
-        
-        
+
     def getAngleParcouru(self):
         return self.angle_parcouru
 
@@ -82,4 +87,3 @@ class RobotSimule(Robot):
     def reset_tracking(self):
         self.distance_parcourue = 0
         self.angle_parcouru = 0
-
