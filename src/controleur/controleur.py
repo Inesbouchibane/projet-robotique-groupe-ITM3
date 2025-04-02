62df8465dc4b6e80c92799674cfbf5e33e647082
Meriem <meriembrh6@gmail.com>
2025-04-02 09:33:42 +0200

    Modification du controleur

--- src/controleur/controleur.py
+++ src/controleur/controleur.py
@@ -1,41 +1,37 @@
-from logging import getLogger
 from threading import Thread
 from time import sleep
-from src.utils import TIC_SIMULATION
+from logging import getLogger
+from src.utils import TIC_CONTROLEUR
+from .strategies import setStrategieCarre, StrategieAuto
 
 class Controler:
-    """Classe permettant de gérer l'exécution des stratégies du robot."""
-
-    def __init__(self):
-        """Initialisation du contrôleur."""
+    def __init__(self, adaptateur=None):
         self.logger = getLogger(self.__class__.__name__)
-        self.strat_en_cours = None
-        self.strategie = False  # Indique si une stratégie est active
+        self.strategie = None
+        self.running = False
+        self.adaptateur = adaptateur
+        
+    def set_strategie(self, strategie_type, **kwargs):
+        if strategie_type == "tracer_carre":
+            self.strategie = setStrategieCarre(kwargs.get('longueur_cote', 100))
+        elif strategie_type == "auto":
+            self.strategie = StrategieAuto(kwargs.get('vitAngG', 0), kwargs.get('vitAngD', 0))
+
+    def lancerStrategie(self, strategie=None):
+        if strategie:
+            self.strategie = strategie
         self.running = True
-        Thread(target=self.mainControleur, daemon=True).start()
+        if self.strategie is not None and self.adaptateur is not None:
+            Thread(target=self.run_strategie, daemon=True).start()
 
-    def mainControleur(self):
-        """Boucle principale exécutant la stratégie active."""
-        while self.running:
-            if self.strategie and self.strat_en_cours:
-                self.logger.debug("Contrôleur actif, exécution d'un step.")
-                if not self.strat_en_cours.stop():
-                    self.strat_en_cours.step()
-                else:
-                    self.logger.debug("Stratégie terminée, arrêt du contrôleur.")
-                    self.strategie = False
-                    self.strat_en_cours.robA.setVitAngA(0)
-                    self.strat_en_cours = None
-            sleep(TIC_SIMULATION)
+    def run_strategie(self):
+        if self.strategie is not None:
+            self.strategie.start(self.adaptateur)
+            while self.running and not self.strategie.stop(self.adaptateur):
+                self.strategie.step(self.adaptateur)
+                sleep(TIC_CONTROLEUR)
+            self.running = False
+            self.logger.debug("Stratégie terminée.")
 
-    def lancerStrategie(self, strat):
-        """Lance une nouvelle stratégie si aucune n'est en cours."""
-        if self.strategie:
-            self.logger.error("Impossible de lancer une nouvelle stratégie : contrôleur occupé.")
-            return False
-        self.strat_en_cours = strat
-        self.strategie = True
-        self.strat_en_cours.start()
-        self.logger.debug("Nouvelle stratégie lancée.")
-        return True
-
+    def stop(self):
+        self.running = False
