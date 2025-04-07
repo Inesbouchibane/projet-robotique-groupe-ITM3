44facf7ee1cb1008d4ccdd26ebb349ae37c402cd
Meriem <meriembrh6@gmail.com>
2025-04-07 11:09:31 +0200

    Suppression des obstacles d'essai

--- src/utils.py
+++ src/utils.py
@@ -11,13 +11,10 @@ VIT_ANG_TOUR = 1  # Angular speed for turns (radians/sec)
 # Obstacle 1 : Rectangle
 LIST_PTS_OBS_RECTANGLE1 = [(450, 375), (550, 375), (550, 425), (450, 425)]
 
-#on le met au milieu 
+#Obstacle2 : Triangle : on le met au milieu 
 LIST_PTS_OBS_TRIANGLE = [(475, 225), (525, 275), (500, 250)]
 
-LIST_PTS_OBS_CARRE2 = [(150, 100), (100, 100), (100, 100), (100, 100)]
-LIST_PTS_OBS_RECTANGLE2 = [(200, 300), (200, 100), (200, 150), (100, 150)]
  
-# Obstacle 3 : Cercle (approximation avec 8 points)
 def generate_circle_points(center_x, center_y, radius, num_points=8):
     points = []
     for i in range(num_points):
@@ -27,7 +24,9 @@ def generate_circle_points(center_x, center_y, radius, num_points=8):
         points.append((x, y))
     return points
 
-LIST_PTS_OBS_CERCLE = generate_circle_points(650, 325, 50)
+#Obstacle 3 : Cercle -> modification que des valeurs pas de la forme des 
+obstacles
+LIST_PTS_OBS_CERCLE = generate_circle_points(500, 100, 30)
 
 def getDistanceFromPts(pt1, pt2):
     return math.sqrt((pt2[0] - pt1[0])**2 + (pt2[1] - pt1[1])**2)
